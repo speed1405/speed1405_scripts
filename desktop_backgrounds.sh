e8073cd00ce60bf2dfd5429bb623ab427d4e0880
speed1405 <36902729+speed1405@users.noreply.github.com>
2018-03-03 14:06:19 +1100

    updated background script

--- desktop_backgrounds.sh
+++ desktop_backgrounds.sh
@@ -1,22 +1,25 @@
 #! /bin/bash
-
 sudo apt-get install xfce4 -yy
 sudo apt-get install xubuntu-desktop -yy
 sudo apt-get install lxde -yy
 sleep 5
-sudo add-apt-repository ppa:gnome3-team/gnome3
+sudo add-apt-repository ppa:gnome3-team/gnome3 -y
 sleep 5
 sudo apt-get install gnome-shell -yy
 sudo apt-get install ubuntu-gnome-desktop -yy
-sudo add-apt-repository ppa:ubuntu-mate-dev/xenial-mate
+sudo add-apt-repository ppa:ubuntu-mate-dev/xenial-mate -y
+sudo apt-get install mate -yy
 sudo apt-get install mate-dock-applet -yy
 sleep 10
-sudo add-apt-repository ppa:budgie-remix/ppa
+sudo add-apt-repository ppa:budgie-remix/ppa -y
 sudo apt-get install budgie-desktop -yy
 sleep 5
 sudo apt-get install awesome -yy
-sudo apt-add-repository ppa:tycho-s/ppa
+sudo apt-add-repository ppa:tycho-s/ppa -y
 sudo apt-get install qtile -yy
-sudo add-apt-repository ppa:razor-qt
+sudo add-apt-repository ppa:razor-q -y
 sudo apt-get install razorqt -yy
-sudo apt-get update
+sudo apt-get install cinnamon-desktop-environment -yy
+sudo add-apt-repository ppa:kubuntu-ppa/backports -y
+sudo apt-get install kubuntu-desktop -yy
+sudo apt-get update -y
