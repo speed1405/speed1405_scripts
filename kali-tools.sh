ff4b87525f999e9e4fbe5bd93da7eb730041521e
speed1405 <36902729+speed1405@users.noreply.github.com>
2018-03-14 19:11:24 +1100

    kali tools

---
+++ kali-tools.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sudo apt-get install git -yy
+git clone https://github.com/LionSec/katoolin.git && cp katoolin/katoolin.py /usr/bin/katoolin
+chmod +x  /usr/bin/katoolin
+sudo katoolin
