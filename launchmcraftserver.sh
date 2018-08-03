762077e1bc3574d18ac7ca3cd1c33c364809111b
speed1405 <36902729+speed1405@users.noreply.github.com>
2018-08-03 12:06:16 +1000

    update
    launcher for mcraft server

--- launchmcraftserver.sh
+++ launchmcraftserver.sh
@@ -4,7 +4,7 @@
 cd minecraft_server
 
 echo "eula=true" > eula.txt
-java -Xmx1024M -Xms1024M -jar minecraft_server.1.12.2.jar 
+java -Xmx1024M -Xms1024M -jar minecraft_server.1.13.jar 
 
 screen -S "Minecraft server"
  
