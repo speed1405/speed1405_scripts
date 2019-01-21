ede3321902185df053a6540085deea085bf0b787
speed1405 <36902729+speed1405@users.noreply.github.com>
2019-01-21 19:04:32 +1100

    update

--- minecraftserver.sh
+++ minecraftserver.sh
@@ -5,15 +5,15 @@ sudo apt-get install default-jdk -yy
 
 sudo apt-get install screen -yy
 
-mkdir minecraft_server
-cd minecraft_server
+mkdir ~/home/minecraft_server
+cd ~/home/minecraft_server
 
 wget https://s3.amazonaws.com/Minecraft.Download/versions/1.13/minecraft_server.1.13.2.jar 
 
 echo "eula=true" > eula.txt
-chmod 777 /home/sean/minecraft_server/minecraft_server.1.13.2.jar
+chmod 777 ~/home/minecraft_server/minecraft_server.1.13.2.jar
 
-java -jar /home/sean/minecraft_server/minecraft_server.1.13.2.jar
+java -jar ~/home/minecraft_server/minecraft_server.1.13.2.jar
 java -Xmx1024M -Xms1024M -jar minecraft_server.1.13.2.jar 
 
 screen -S "Minecraft server"
