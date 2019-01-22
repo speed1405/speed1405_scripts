8a888a7db1b0584d5908806872f353aa6d677277
speed1405 <36902729+speed1405@users.noreply.github.com>
2019-01-22 12:08:05 +1100

    update

--- minecraftinstall.sh
+++ minecraftinstall.sh
@@ -11,11 +11,11 @@ sleep 10
 
 sudo apt install minecraft-installer 
 
-mkdir minecraft_portable
-cd minecraft_portable
+mkdir ~/home/minecraft_portable
+cd ~/home/minecraft_portable
 
 wget http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
 
-chmod 777 /home/sean/minecraft_portable/Minecraft.jar
+chmod 777 ~/home/minecraft_portable/Minecraft.jar
 
-java -jar /home/sean/minecraft_portable/Minecraft.jar
+java -jar ~/home/minecraft_portable/Minecraft.jar
